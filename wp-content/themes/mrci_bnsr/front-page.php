d49d6c8ee9550db037251cf7dee21c873d6780e6
Léa <lea@jesuisunebouledisco.local>
2024-09-09 08:56:13 +0200

    plugins +  wp update + basic config + front page

--- wp-content/themes/mrci_bnsr/front-page.php
+++ wp-content/themes/mrci_bnsr/front-page.php
@@ -1,7 +1,24 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<div>MERCI BONSOIR.</div>
+<div class= "front-page-bg">
+	<?php 
+		$image = get_field('logo');
+		$size = 'full'; // (thumbnail, medium, large, full or custom size)
+		if( $image ) {
+		    echo wp_get_attachment_image( $image, $size );
+		}
+	<?php>
+	<?php 
+		$link = get_field('listen');
+		if( $link ): 
+		    $link_url = $link['url'];
+		    $link_title = $link['title'];
+		    $link_target = $link['target'] ? $link['target'] : '_self';
+		    ?>
+		    <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+	<?php endif; ?>
+</div>
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
